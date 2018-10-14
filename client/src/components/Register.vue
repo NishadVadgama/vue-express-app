68c322ef2df595fbd5e900a1db9ee4ef0c21d2e2
Nishad Vadgama <nishad.vadgama@gmail.com>
2018-10-14 19:45:20 +0530

    Client Server basic Connectivity tested

---
+++ client/src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="email" v-model="email" name="email" placeholder="email">
+    <input type="password" v-model="password" name="password" placeholder="password">
+    <button @click="register">Submit</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: null,
+      password: null
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
